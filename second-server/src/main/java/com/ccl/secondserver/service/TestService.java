963aa782373904de9040831ef4abb7225d5f408c
迟成龙 <497455606@qq.com>
2020-11-12 11:35:45 +0800

    LRU算法添加

--- second-server/src/main/java/com/ccl/secondserver/service/TestService.java
+++ second-server/src/main/java/com/ccl/secondserver/service/TestService.java
@@ -1,4 +1,6 @@
 package com.ccl.secondserver.service;
 
 public interface TestService {
+
+
 }
--- study-server/src/main/java/com/ccl/studyserver/arithmetic/lru/Node.java
+++ study-server/src/main/java/com/ccl/studyserver/arithmetic/lru/Node.java
@@ -15,4 +15,17 @@ package com.ccl.studyserver.arithmetic.lru;
  * ****    ****    ****
  */
 public class Node {
+    //键
+    Object key;
+    //值
+    Object value;
+    //上一个节点
+    Node pre;
+    //下一个节点
+    Node next;
+
+    public Node(Object key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
 }
